ae23351bc6bc6ffb6f6b29ae5014c672b37f310a
Vivek Aswal <vivek.aswal@learningmate.com>
2020-03-22 17:19:07 -0400

    React Hook + Contect API

---
+++ contextapp/src/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import BookList from './components/BookList';
+import Navbar from './components/Navbar';
+import ThemeContextProvider from './contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+        <ThemeToggle />
+      </ThemeContextProvider>
+    </div>
+  );
+}
+
+export default App;
