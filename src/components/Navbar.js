ab6ff14739faa23be8d9e3f0260f901c97dbae2f
Vivek Aswal <vivek.software.engg@gmail.com>
2020-04-23 00:41:33 -0400

    Check change for amplify

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ class Navbar extends Component {
     const theme = isLightTheme ? light : dark;
     return ( 
       <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context App</h1>
+        <h1>Amplify App</h1>
         <ul>
           <li>Home</li>
           <li>About</li>
